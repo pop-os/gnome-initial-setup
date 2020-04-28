520c0fb69f66e95dce362b12076c204758383fdb
Jeremy Soller <jeremy@system76.com>
2020-04-28 12:27:01 -0600

    Disable theme-switcher again (#63)

--- gnome-initial-setup/gnome-initial-setup.c
+++ gnome-initial-setup/gnome-initial-setup.c
@@ -66,7 +66,7 @@ typedef struct {
 static PageData page_table[] = {
   PAGE (language, FALSE),
   PAGE (keyboard, FALSE),
-  PAGE (appearance, FALSE),
+//  PAGE (appearance, FALSE),
   PAGE (network,  FALSE),
   PAGE (privacy,  FALSE),
   PAGE (timezone, TRUE),
