pop-os/gnome-initial-setup

commit f220437a419706d05c31b1669c9ae6702dfd0363

summary: Update note about default graphics mode
Only oryp4 will default to integrated graphics mode. All models with
Turing and newer cards will default to hybrid graphics mode.

gnome-initial-setup/pages/summary/gis-summary-page.c
@@ -45,6 +45,7 @@ struct _GisSummaryPagePrivate {
   GtkWidget *left_panel_label;
   GtkWidget *left_panel_image;
   GtkWidget *left_panel_description;
+  GtkWidget *right_panel_image;
   GtkWidget *right_panel_label;
   GtkWidget *right_panel_description;
 
@@ -336,9 +337,20 @@ gis_summary_page_set_switchable_descriptions (GisSummaryPagePrivate *priv, char 
     "Integrated, NVIDIA, and Hybrid Graphics. Switching will prompt you to "
     "restart your device.");
 
-  char *right_desc = g_strdup_printf (_("To increase battery life, your %s "
-    "defaults to Integrated graphics. To use external displays, switch to NVIDIA "
-    "graphics."), product_name);
+  // oryp4 does not have a Turing card, so it defaults to integrated
+  char *right_desc;
+  if (strcmp (product_name, "oryp4") == 0 ||
+      strcmp (product_name, "oryp4-b") == 0) {
+    gtk_label_set_label (GTK_LABEL (priv->right_panel_label), _("Using External Displays"));
+
+    right_desc = g_strdup_printf (_("To increase battery life, your %s "
+      "defaults to Integrated graphics. To use external displays, switch to NVIDIA "
+      "graphics."), product_name);
+  } else {
+    right_desc = g_strdup_printf (_("Your %s defaults to Hybrid graphics. To launch "
+      "an application on the NVIDIA GPU, right click the desktop icon and select "
+      "\"Launch using Dedicated Graphics Card\"."), product_name);
+  }
 
   gtk_label_set_line_wrap (GTK_LABEL (priv->left_panel_description), 1);
   gtk_label_set_label (GTK_LABEL (priv->left_panel_description), left_desc);
@@ -350,7 +362,7 @@ gis_summary_page_set_switchable_descriptions (GisSummaryPagePrivate *priv, char 
 }
 
 static void
-gis_summary_page_scale_switchable_images (GisSummaryPagePrivate *priv)
+gis_summary_page_scale_switchable_images (GisSummaryPagePrivate *priv, char *product_name)
 {
   GtkImage *left_image = GTK_IMAGE (priv->left_panel_image);
   gint scale_w = 250;
@@ -365,6 +377,24 @@ gis_summary_page_scale_switchable_images (GisSummaryPagePrivate *priv)
       GDK_INTERP_BILINEAR
     )
   );
+
+  GtkImage *right_image = GTK_IMAGE (priv->right_panel_image);
+  if (strcmp (product_name, "oryp4") == 0 ||
+      strcmp (product_name, "oryp4-b") == 0) {
+    gtk_image_set_from_icon_name (right_image, "video-display", GTK_ICON_SIZE_DIALOG);
+    gtk_image_set_pixel_size (right_image, 256);
+  } else {
+    gtk_image_set_from_pixbuf (
+      right_image,
+      gdk_pixbuf_scale_simple (
+        gtk_image_get_pixbuf (right_image),
+        425,  // scale_w
+        180,  // scale_h
+        GDK_INTERP_BILINEAR
+      )
+    );
+  }
+
 }
 
 static void
@@ -388,7 +418,7 @@ gis_summary_page_constructed (GObject *object)
         gis_summary_page_set_switchable_descriptions (priv, product_name);
     }
 
-    gis_summary_page_scale_switchable_images (priv);
+    gis_summary_page_scale_switchable_images (priv, product_name);
   }
 
   update_distro_name (page);
@@ -429,6 +459,7 @@ gis_summary_page_class_init (GisSummaryPageClass *klass)
     gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), GisSummaryPage, left_panel_image);
     gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), GisSummaryPage, left_panel_description);
     gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), GisSummaryPage, right_panel_label);
+    gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), GisSummaryPage, right_panel_image);
     gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), GisSummaryPage, right_panel_description);
   } else {
     gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), GisSummaryPage, tagline);

gnome-initial-setup/pages/summary/gis-summary-page-oryx-switchable.ui
@@ -84,11 +84,10 @@
                         </packing>
                     </child>
                     <child>
-                        <object class="GtkImage">
+                        <object class="GtkImage" id="right_panel_image">
                             <property name="visible">True</property>
                             <property name="halign">center</property>
-                            <property name="icon-name">video-display</property>
-                            <property name="pixel-size">256</property>
+                            <property name="resource">/org/gnome/initial-setup/right-click.png</property>
                         </object>
                         <packing>
                             <property name="left-attach">1</property>
@@ -100,7 +99,7 @@
                             <property name="halign">center</property>
                             <property name="visible">True</property>
                             <property name="use-markup">True</property>
-                            <property name="label" translatable="yes">Using External Displays</property>
+                            <property name="label" translatable="yes">Using NVIDIA Graphics</property>
                             <attributes>
                               <attribute name="weight" value="bold"/>
                             </attributes>

gnome-initial-setup/pages/summary/summary.gresource.xml
@@ -5,5 +5,6 @@
     <file preprocess="xml-stripblanks" alias="gis-summary-page-oryx-switchable.ui">gis-summary-page-oryx-switchable.ui</file>
     <file alias="ready-to-go.svg">ready-to-go.svg</file>
     <file alias="switching.png">switching.png</file>
+    <file alias="right-click.png">right-click.png</file>
   </gresource>
 </gresources>

commit c6deb91aecd310e576b49cc227dd70e5706e8f58

summary: Fix comparison for oryp4
The previous change used the product *name* for the comparison, when it
should use the product *version*. Pass the trimmed version to the
functions that need it.

Fixes: f220437a4197 ("summary: Update note about default graphics mode")

gnome-initial-setup/pages/summary/gis-summary-page.c
@@ -293,10 +293,22 @@ trim (char *str)
   return str;
 }
 
+static char*
+get_product_name ()
+{
+  return freadln ("/sys/class/dmi/id/product_name");
+}
+
+static char*
+get_product_version ()
+{
+  return freadln ("/sys/class/dmi/id/product_version");
+}
+
 static u_int8_t
 has_switchable_graphics ()
 {
-  char *product_version = freadln ("/sys/class/dmi/id/product_version");
+  char *product_version = get_product_version();
   u_int8_t has_switchable = 0;
 
   if (product_version) {
@@ -316,12 +328,6 @@ has_switchable_graphics ()
   return has_switchable;
 }
 
-static char*
-get_product_name ()
-{
-  return freadln ("/sys/class/dmi/id/product_name");
-}
-
 static void
 gis_summary_page_set_switchable_title (GisSummaryPagePrivate *priv, char *product_name)
 {
@@ -331,7 +337,7 @@ gis_summary_page_set_switchable_title (GisSummaryPagePrivate *priv, char *produc
 }
 
 static void
-gis_summary_page_set_switchable_descriptions (GisSummaryPagePrivate *priv, char *product_name)
+gis_summary_page_set_switchable_descriptions (GisSummaryPagePrivate *priv, char *product_name, char *product_version)
 {
   char *left_desc = _("Use the system menu on the top panel to switch between "
     "Integrated, NVIDIA, and Hybrid Graphics. Switching will prompt you to "
@@ -339,8 +345,8 @@ gis_summary_page_set_switchable_descriptions (GisSummaryPagePrivate *priv, char 
 
   // oryp4 does not have a Turing card, so it defaults to integrated
   char *right_desc;
-  if (strcmp (product_name, "oryp4") == 0 ||
-      strcmp (product_name, "oryp4-b") == 0) {
+  if (strcmp (product_version, "oryp4") == 0 ||
+      strcmp (product_version, "oryp4-b") == 0) {
     gtk_label_set_label (GTK_LABEL (priv->right_panel_label), _("Using External Displays"));
 
     right_desc = g_strdup_printf (_("To increase battery life, your %s "
@@ -362,7 +368,7 @@ gis_summary_page_set_switchable_descriptions (GisSummaryPagePrivate *priv, char 
 }
 
 static void
-gis_summary_page_scale_switchable_images (GisSummaryPagePrivate *priv, char *product_name)
+gis_summary_page_scale_switchable_images (GisSummaryPagePrivate *priv, char *product_version)
 {
   GtkImage *left_image = GTK_IMAGE (priv->left_panel_image);
   gint scale_w = 250;
@@ -379,8 +385,8 @@ gis_summary_page_scale_switchable_images (GisSummaryPagePrivate *priv, char *pro
   );
 
   GtkImage *right_image = GTK_IMAGE (priv->right_panel_image);
-  if (strcmp (product_name, "oryp4") == 0 ||
-      strcmp (product_name, "oryp4-b") == 0) {
+  if (strcmp (product_version, "oryp4") == 0 ||
+      strcmp (product_version, "oryp4-b") == 0) {
     gtk_image_set_from_icon_name (right_image, "video-display", GTK_ICON_SIZE_DIALOG);
     gtk_image_set_pixel_size (right_image, 256);
   } else {
@@ -406,19 +412,23 @@ gis_summary_page_constructed (GObject *object)
   G_OBJECT_CLASS (gis_summary_page_parent_class)->constructed (object);
 
   if (has_switchable_graphics ()) {
+    char *product_version = get_product_version ();
+    char *version = trim(product_version);
+
     char *product_name = get_product_name ();
     if (product_name) {
         char *trimmed = trim (product_name);
         gis_summary_page_set_switchable_title (priv, trimmed);
-        gis_summary_page_set_switchable_descriptions (priv, trimmed);
+        gis_summary_page_set_switchable_descriptions (priv, trimmed, version);
         free (product_name);
     } else {
         product_name = _("System");
         gis_summary_page_set_switchable_title (priv, product_name);
-        gis_summary_page_set_switchable_descriptions (priv, product_name);
+        gis_summary_page_set_switchable_descriptions (priv, product_name, version);
     }
 
-    gis_summary_page_scale_switchable_images (priv, product_name);
+    gis_summary_page_scale_switchable_images (priv, version);
+    free (product_version);
   }
 
   update_distro_name (page);